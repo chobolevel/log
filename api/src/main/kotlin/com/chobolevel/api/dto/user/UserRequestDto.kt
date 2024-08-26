fcd541fb7b374ed4ef0f46258edf84e35ed5239a
chobolevel <rodaka123@naver.com>
2024-08-26 23:07:05 +0900

    Fix: lint issue

--- api/src/main/kotlin/com/chobolevel/api/dto/user/UserRequestDto.kt
+++ api/src/main/kotlin/com/chobolevel/api/dto/user/UserRequestDto.kt
@@ -4,7 +4,6 @@ import com.chobolevel.domain.entity.user.UserLoginType
 import com.chobolevel.domain.entity.user.UserUpdateMask
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
-import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
