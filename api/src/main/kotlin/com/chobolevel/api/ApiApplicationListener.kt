cc2684257785cdced6b0e72c73fd469a9ed2a54e
chobolevel <rodaka123@naver.com>
2024-11-19 14:12:52 +0900

    Fix: lint issue

--- api/src/main/kotlin/com/chobolevel/api/ApiApplicationListener.kt
+++ api/src/main/kotlin/com/chobolevel/api/ApiApplicationListener.kt
@@ -1,13 +1,11 @@
 package com.chobolevel.api
 
-import com.chobolevel.api.controller.v1.post.PostController
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
-import org.springframework.web.client.getForObject
 
 @Component
 class ApiApplicationListener(
