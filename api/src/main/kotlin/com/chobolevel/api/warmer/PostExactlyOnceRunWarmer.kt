ed6a81b0c730ba8e3ccf8aa3ed92f8002650fb21
chobolevel <rodaka123@naver.com>
2024-11-20 16:34:49 +0900

    Refactor: modify jvm warm up logic

--- api/src/main/kotlin/com/chobolevel/api/warmer/PostExactlyOnceRunWarmer.kt
+++ api/src/main/kotlin/com/chobolevel/api/warmer/PostExactlyOnceRunWarmer.kt
@@ -1,24 +1,24 @@
-package com.chobolevel.api.warmer
-
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Component
-import org.springframework.web.client.RestTemplate
-import org.springframework.web.client.getForEntity
-
-@Component
-class PostExactlyOnceRunWarmer(
-    private val restTemplate: RestTemplate,
-    @Value("\${server.host}")
-    private val host: String
-) : ExactlyOnceRunWarmer() {
-
-    private val logger = LoggerFactory.getLogger(PostExactlyOnceRunWarmer::class.java)
-
-    override suspend fun doRun() {
-        logger.info("===== warm up started with $host ====")
-        restTemplate.getForEntity<String>("$host/api/v1/posts")
-        restTemplate.getForEntity<String>("$host/api/v1/posts/1")
-        logger.info("===== warm up ended with $host ======")
-    }
-}
+//package com.chobolevel.api.warmer
+//
+//import org.slf4j.LoggerFactory
+//import org.springframework.beans.factory.annotation.Value
+//import org.springframework.stereotype.Component
+//import org.springframework.web.client.RestTemplate
+//import org.springframework.web.client.getForEntity
+//
+//@Component
+//class PostExactlyOnceRunWarmer(
+//    private val restTemplate: RestTemplate,
+//    @Value("\${server.host}")
+//    private val host: String
+//) : ExactlyOnceRunWarmer() {
+//
+//    private val logger = LoggerFactory.getLogger(PostExactlyOnceRunWarmer::class.java)
+//
+//    override suspend fun doRun() {
+//        logger.info("===== warm up started with $host ====")
+//        restTemplate.getForEntity<String>("$host/api/v1/posts")
+//        restTemplate.getForEntity<String>("$host/api/v1/posts/1")
+//        logger.info("===== warm up ended with $host ======")
+//    }
+//}
