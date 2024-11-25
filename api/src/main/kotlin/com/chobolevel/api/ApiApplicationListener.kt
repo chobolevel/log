6e126c482a29933bc897c9224bc88ff1953ac5ca
chobolevel <rodaka123@naver.com>
2024-11-25 16:36:37 +0900

    Fix: warm up API URL issue

--- api/src/main/kotlin/com/chobolevel/api/ApiApplicationListener.kt
+++ api/src/main/kotlin/com/chobolevel/api/ApiApplicationListener.kt
@@ -7,21 +7,22 @@ import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
-import java.time.OffsetDateTime
+import java.net.InetAddress
 
 @Component
 class ApiApplicationListener(
     private val restTemplate: RestTemplate,
-    @Value("\${server.host}")
-    private val host: String
+    @Value("\${server.protocol}")
+    private val protocol: String
 ) : ApplicationListener<ApplicationReadyEvent> {
 
     private val logger = LoggerFactory.getLogger(ApiApplication::class.java)
 
     override fun onApplicationEvent(event: ApplicationReadyEvent) {
-        logger.info("===== [${OffsetDateTime.now()}]warm up started with $host ====")
-        restTemplate.getForEntity<String>("$host/api/v1/posts")
-        restTemplate.getForEntity<String>("$host/api/v1/posts/1")
-        logger.info("===== [${OffsetDateTime.now()}]warm up ended with $host ======")
+        val url = "${protocol}://${InetAddress.getLocalHost().hostAddress}:9565"
+        logger.info("===== warm up started with $url ====")
+        restTemplate.getForEntity<String>("$url/api/v1/posts")
+        restTemplate.getForEntity<String>("$url/api/v1/posts/1")
+        logger.info("===== warm up ended with $url ======")
     }
 }
--- api/src/main/kotlin/com/chobolevel/api/scheduler/WarmScheduler.kt
+++ api/src/main/kotlin/com/chobolevel/api/scheduler/WarmScheduler.kt
@@ -7,12 +7,13 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
+import java.net.InetAddress
 
 @Component
 class WarmScheduler(
     private val restTemplate: RestTemplate,
-    @Value("\${server.host}")
-    private val host: String
+    @Value("\${server.protocol}")
+    private val protocol: String
 ) {
 
     private val logger = LoggerFactory.getLogger(WarmScheduler::class.java)
@@ -20,9 +21,10 @@ class WarmScheduler(
     @Async
     @Scheduled(cron = "0 */10 * * * *")
     fun postControllerWarmer() {
-        logger.info("===== warm up scheduler started with $host ====")
-        restTemplate.getForEntity<String>("$host/api/v1/posts")
-        restTemplate.getForEntity<String>("$host/api/v1/posts/1")
-        logger.info("===== warm up scheduler ended with $host ======")
+        val url = "${protocol}://${InetAddress.getLocalHost().hostAddress}:9565"
+        logger.info("===== warm up started with $url ====")
+        restTemplate.getForEntity<String>("$url/api/v1/posts")
+        restTemplate.getForEntity<String>("$url/api/v1/posts/1")
+        logger.info("===== warm up ended with $url ======")
     }
 }
