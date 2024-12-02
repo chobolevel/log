6cc6bd3e502711376d83e4e1152605b71f4635d6
chobolevel <rodaka123@naver.com>
2024-12-03 00:20:33 +0900

    Feat: add WebSocket connection request interceptor

---
+++ api/src/main/kotlin/com/chobolevel/api/config/ChannelAuthorizationInterceptor.kt
@@ -0,0 +1,43 @@
+package com.chobolevel.api.config
+
+import com.chobolevel.api.security.TokenProvider
+import com.chobolevel.domain.exception.ApiException
+import com.chobolevel.domain.exception.ErrorCode
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Primary
+import org.springframework.http.HttpStatus
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageChannel
+import org.springframework.messaging.simp.stomp.StompCommand
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor
+import org.springframework.messaging.support.ChannelInterceptor
+import org.springframework.stereotype.Component
+
+@Primary
+@Component
+class ChannelAuthorizationInterceptor(
+    private val tokenProvider: TokenProvider
+) : ChannelInterceptor {
+
+    private final val PREFIX = "Bearer "
+
+    override fun preSend(message: Message<*>, channel: MessageChannel): Message<*>? {
+        val accessor = StompHeaderAccessor.wrap(message)
+        if (accessor.command == StompCommand.CONNECT) {
+            val accessToken = accessor.getFirstNativeHeader("Authorization")
+            if (accessToken == null || !accessToken.startsWith(PREFIX)) {
+                // TODO Authentication exception
+                throw ApiException(
+                    errorCode = ErrorCode.ALREADY_EXITED_CHANNEL,
+                    status = HttpStatus.UNAUTHORIZED,
+                    message = "사용 금지"
+                )
+            }
+            val authentication = tokenProvider.getAuthentication(accessToken.substring(PREFIX.length))
+            if (authentication == null) {
+                // TODO Authentication exception
+            }
+        }
+        return message
+    }
+}
--- api/src/main/kotlin/com/chobolevel/api/config/WebSocketConfiguration.kt
+++ api/src/main/kotlin/com/chobolevel/api/config/WebSocketConfiguration.kt
@@ -1,6 +1,7 @@
 package com.chobolevel.api.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
@@ -8,14 +9,25 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketConfiguration : WebSocketMessageBrokerConfigurer {
+class WebSocketConfiguration(
+    private val authorizationInterceptor: ChannelAuthorizationInterceptor
+) : WebSocketMessageBrokerConfigurer {
 
     override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        // WebSocket 연결을 위한 엔드포인트 설정
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*")
+        // 브라우저 이슈로 WebSocket 연결이 불가능한 경우 SockJS 통한 연결을 지원
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS()
     }
 
     override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        // 메세지를 구독하는 요청 엔드포인트
         registry.enableSimpleBroker("/sub")
+        // 메시지를 발생하는 요청 엔드포인트
         registry.setApplicationDestinationPrefixes("/pub")
+    }
+
+    override fun configureClientInboundChannel(registration: ChannelRegistration) {
+        registration.interceptors(authorizationInterceptor)
     }
 }
