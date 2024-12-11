b6a21fc251c189039775e67b14cec8c68b070ed0
chobolevel <rodaka123@naver.com>
2024-12-11 15:09:09 +0900

    Refactor: modify clients table schema

--- domain/src/main/resources/db/migration/V2.1__add_clients_table.sql
+++ domain/src/main/resources/db/migration/V2.1__add_clients_table.sql
@@ -1,20 +1,28 @@
 create table log.clients
 (
-  client_id     varchar(100) not null
+  id           varchar(100) not null
     primary key,
-  client_secret varchar(100) not null,
-  created_at    datetime     not null,
-  updated_at    datetime     not null
+  user_id      bigint       not null,
+  secret       varchar(100) not null,
+  name         varchar(100) not null,
+  redirect_url varchar(255) not null,
+  deleted      bit          not null,
+  created_at   datetime     not null,
+  updated_at   datetime     not null
 ) comment '클라이언트 키 관리 테이블';
 
 create table log.clients_histories
 (
-  client_id     varchar(100) not null,
-  rev_id        bigint       not null,
-  revtype       tinyint      not null,
-  client_secret varchar(100) not null,
-  created_at    datetime     not null,
-  updated_at    datetime     not null,
-  primary key (client_id, rev_id)
+  id           varchar(100) not null,
+  rev_id       bigint       not null,
+  revtype      tinyint      not null,
+  user_id      bigint       not null,
+  secret       varchar(100) not null,
+  name         varchar(100) not null,
+  redirect_url varchar(255) not null,
+  deleted      bit          not null,
+  created_at   datetime     not null,
+  updated_at   datetime     not null,
+  primary key (id, rev_id)
 ) comment '클라이언트 키 관리 테이블 히스토리';
 
