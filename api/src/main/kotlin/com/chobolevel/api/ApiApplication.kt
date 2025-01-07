e5a6a47dc6c7b88395af27c0e11546b95757bc02
chobolevel <rodaka123@naver.com>
2025-01-07 14:07:43 +0900

    Refactor: modify logback file to use spring property

--- api/src/main/kotlin/com/chobolevel/api/ApiApplication.kt
+++ api/src/main/kotlin/com/chobolevel/api/ApiApplication.kt
@@ -1,10 +1,11 @@
 package com.chobolevel.api
 
 import com.chobolevel.domain.DomainConfigurationLoader
+import com.ulisesbocchio.jasyptspringboot.environment.StandardEncryptableEnvironment
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.scheduling.annotation.EnableAsync
@@ -20,5 +21,9 @@ import org.springframework.scheduling.annotation.EnableScheduling
 class ApiApplication
 
 fun main(args: Array<String>) {
-    runApplication<ApiApplication>(*args)
+    // logback jasypt 변수 사용하기 위해 ApiApplication 실행 시점을 변경
+    SpringApplicationBuilder()
+        .environment(StandardEncryptableEnvironment())
+        .sources(ApiApplication::class.java)
+        .run(*args)
 }
