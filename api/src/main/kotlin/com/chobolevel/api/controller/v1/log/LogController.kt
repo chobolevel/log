db36045337eca289134ed825cde587424af43058
chobolevel <rodaka123@naver.com>
2025-01-09 15:42:36 +0900

    Fix: cant read linux file issue

--- api/src/main/kotlin/com/chobolevel/api/controller/v1/log/LogController.kt
+++ api/src/main/kotlin/com/chobolevel/api/controller/v1/log/LogController.kt
@@ -6,6 +6,7 @@ import com.chobolevel.domain.exception.ApiException
 import com.chobolevel.domain.exception.ErrorCode
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.io.BufferedReader
 import java.io.FileReader
+import java.io.IOException
 
 @Tag(name = "Log (로그)", description = "로그 관리 API")
 @RestController
@@ -23,6 +25,8 @@ class LogController(
     private val nginxLogPath: String
 ) {
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @Operation(summary = "nginx log 조회 API")
     @HasAuthorityAdmin
     @GetMapping("/nginx")
@@ -30,7 +34,8 @@ class LogController(
         try {
             val reader = BufferedReader(FileReader("$nginxLogPath/$filename"))
             return ResponseEntity.ok(ResultResponse(reader.lines().toList()))
-        } catch (e: Exception) {
+        } catch (e: IOException) {
+            logger.error("error occurred while reading file", e)
             throw ApiException(
                 errorCode = ErrorCode.L001,
                 message = "로그 파일을 찾을 수 없습니다."
