63bdecc99622b3472e1ae1a1d994e946b8e9af87
chobolevel <rodaka123@naver.com>
2025-01-11 12:34:28 +0900

    Fix: issue ssl scripts legacy domain naming issue

--- deploy/init-letsencrypt.sh
+++ deploy/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(api.chobolevel.site)
+domains=(api.chobolevel.co.kr)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="rodaka123@naver.com" # Adding a valid address is strongly recommended
