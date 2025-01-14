45fbfdc023822c5872b8ecbca1036d0859a9eef8
chobolevel <rodaka123@naver.com>
2025-01-14 16:59:28 +0900

    Refactor: add password validation

--- api/src/main/kotlin/com/chobolevel/api/service/user/validator/CreateUserValidator.kt
+++ api/src/main/kotlin/com/chobolevel/api/service/user/validator/CreateUserValidator.kt
@@ -10,7 +10,8 @@ import org.springframework.stereotype.Component
 @Component
 class CreateUserValidator : CreateUserValidatable {
 
-    private val emailRegexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$".toRegex()
+    private final val emailRegexp = "^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+\$".toRegex()
+    private final val passwordRegexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#\$%^&*(),.?\":{}|<>]).{8,}\$".toRegex()
 
     override fun validate(request: CreateUserRequestDto) {
         if (!emailRegexp.matches(request.email)) {
@@ -20,21 +21,31 @@ class CreateUserValidator : CreateUserValidatable {
                 message = "이메일 형식이 올바르지 않습니다."
             )
         }
-        if (request.loginType == UserLoginType.GENERAL) {
-            if (request.password.isNullOrBlank()) {
-                throw ApiException(
-                    errorCode = ErrorCode.INVALID_PARAMETER,
-                    status = HttpStatus.BAD_REQUEST,
-                    message = "회원가입 시 비밀번호는 필수 값입니다."
-                )
+        when (request.loginType) {
+            UserLoginType.GENERAL -> {
+                if (request.password.isNullOrBlank()) {
+                    throw ApiException(
+                        errorCode = ErrorCode.INVALID_PARAMETER,
+                        status = HttpStatus.BAD_REQUEST,
+                        message = "회원가입 시 비밀번호는 필수 값입니다."
+                    )
+                }
+                if (!request.password.matches(passwordRegexp)) {
+                    throw ApiException(
+                        errorCode = ErrorCode.INVALID_PARAMETER,
+                        message = "비밀번호는 영문 + 숫자 + 특수문자 조합으로 8자리 이상이어야 합니다."
+                    )
+                }
             }
-        } else {
-            if (request.socialId.isNullOrEmpty()) {
-                throw ApiException(
-                    errorCode = ErrorCode.INVALID_PARAMETER,
-                    status = HttpStatus.BAD_REQUEST,
-                    message = "소셜회원가입 시 소셜 아이디는 필수 값입니다."
-                )
+
+            else -> {
+                if (request.socialId.isNullOrEmpty()) {
+                    throw ApiException(
+                        errorCode = ErrorCode.INVALID_PARAMETER,
+                        status = HttpStatus.BAD_REQUEST,
+                        message = "소셜회원가입 시 소셜 아이디는 필수 값입니다."
+                    )
+                }
             }
         }
     }
