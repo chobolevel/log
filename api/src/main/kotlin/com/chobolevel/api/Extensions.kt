e35855af77de7014ee2b7bbe8759cf27b88fec8f
chobolevel <rodaka123@naver.com>
2025-01-16 14:13:50 +0900

    Refactor: divide duplicated getting cookie logic

--- api/src/main/kotlin/com/chobolevel/api/Extensions.kt
+++ api/src/main/kotlin/com/chobolevel/api/Extensions.kt
@@ -1,7 +1,15 @@
 package com.chobolevel.api
 
+import jakarta.servlet.http.HttpServletRequest
 import java.security.Principal
 
 fun Principal.getUserId(): Long {
     return this.name.toLong()
+}
+
+fun HttpServletRequest.getCookie(key: String): String? {
+    if (this.cookies.isNullOrEmpty() || this.cookies.find { it.name == key } == null) {
+        return null
+    }
+    return this.cookies.find { it.name == key }!!.value
 }
