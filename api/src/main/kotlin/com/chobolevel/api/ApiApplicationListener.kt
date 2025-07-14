86e71a318169b6214698826084a1f9078b500a3b
chobolevel <rodaka123@naver.com>
2025-07-14 17:01:03 +0900

    refactor: modify to warming only on production profile

--- api/src/main/kotlin/com/chobolevel/api/ApiApplicationListener.kt
+++ api/src/main/kotlin/com/chobolevel/api/ApiApplicationListener.kt
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import java.net.InetAddress
 
 @Component
+@Profile("production")
 class ApiApplicationListener(
     private val warmers: List<Warmer>,
     @Qualifier("baseRestTemplate")
