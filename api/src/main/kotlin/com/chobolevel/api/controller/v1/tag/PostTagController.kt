39014750db3e3b430d1b23bfb3a661e585d93ced
chobolevel <rodaka123@naver.com>
2025-07-30 23:37:28 +0900

    refactor: add type declare

--- api/src/main/kotlin/com/chobolevel/api/controller/v1/tag/PostTagController.kt
+++ api/src/main/kotlin/com/chobolevel/api/controller/v1/tag/PostTagController.kt
@@ -1,12 +1,15 @@
 package com.chobolevel.api.controller.v1.tag
 
 import com.chobolevel.api.annotation.HasAuthorityAdmin
+import com.chobolevel.api.dto.common.PaginationResponseDto
 import com.chobolevel.api.dto.common.ResultResponse
 import com.chobolevel.api.dto.tag.CreateTagRequestDto
 import com.chobolevel.api.dto.tag.UpdateTagRequestDto
 import com.chobolevel.api.service.tag.TagService
 import com.chobolevel.api.service.tag.query.TagQueryCreator
 import com.chobolevel.domain.entity.tag.TagOrderType
+import com.chobolevel.domain.entity.tag.TagQueryFilter
+import com.scrimmers.domain.dto.common.Pagination
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
@@ -36,7 +39,7 @@ class PostTagController(
         @Valid @RequestBody
         request: CreateTagRequestDto
     ): ResponseEntity<ResultResponse> {
-        val result = service.createPostTag(request)
+        val result: Long = service.createPostTag(request)
         return ResponseEntity.ok(ResultResponse(result))
     }
 
@@ -48,14 +51,14 @@ class PostTagController(
         @RequestParam(required = false) limitCount: Long?,
         @RequestParam(required = false) orderTypes: List<TagOrderType>?
     ): ResponseEntity<ResultResponse> {
-        val queryFilter = queryCreator.createQueryFilter(
+        val queryFilter: TagQueryFilter = queryCreator.createQueryFilter(
             name = name
         )
-        val pagination = queryCreator.createPaginationFilter(
+        val pagination: Pagination = queryCreator.createPaginationFilter(
             skipCount = skipCount,
             limitCount = limitCount
         )
-        val result = service.searchPostTags(queryFilter, pagination, orderTypes)
+        val result: PaginationResponseDto = service.searchPostTags(queryFilter, pagination, orderTypes)
         return ResponseEntity.ok(ResultResponse(result))
     }
 
@@ -67,7 +70,7 @@ class PostTagController(
         @Valid @RequestBody
         request: UpdateTagRequestDto
     ): ResponseEntity<ResultResponse> {
-        val result = service.updatePostTag(
+        val result: Long = service.updatePostTag(
             postTagId = id,
             request = request
         )
@@ -78,7 +81,7 @@ class PostTagController(
     @HasAuthorityAdmin
     @DeleteMapping("/tags/{id}")
     fun deletePostTag(@PathVariable id: Long): ResponseEntity<ResultResponse> {
-        val result = service.deletePostTag(
+        val result: Boolean = service.deletePostTag(
             postTagId = id
         )
         return ResponseEntity.ok(ResultResponse(result))
