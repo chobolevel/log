8bd7e45cfc497163aa88fde46d5e868e5186702c
chobolevel <rodaka123@naver.com>
2025-08-06 23:25:12 +0900

    refactor: add type declare

--- api/src/main/kotlin/com/chobolevel/api/security/CustomAuthenticationManager.kt
+++ api/src/main/kotlin/com/chobolevel/api/security/CustomAuthenticationManager.kt
@@ -1,5 +1,6 @@
 package com.chobolevel.api.security
 
+import com.chobolevel.domain.entity.user.User
 import com.chobolevel.domain.entity.user.UserFinder
 import com.chobolevel.domain.entity.user.UserLoginType
 import com.chobolevel.domain.exception.ApiException
@@ -9,6 +10,7 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Component
@@ -20,15 +22,15 @@ class CustomAuthenticationManager(
 ) : AuthenticationManager {
 
     override fun authenticate(authentication: Authentication): Authentication {
-        val combine = authentication.name.split("/")
-        val email = combine[0]
-        val loginType = UserLoginType.find(combine[1]) ?: throw ApiException(
+        val combine: List<String> = authentication.name.split("/")
+        val email: String = combine[0]
+        val loginType: UserLoginType = UserLoginType.find(combine[1]) ?: throw ApiException(
             errorCode = ErrorCode.INVALID_PARAMETER,
             status = HttpStatus.BAD_REQUEST,
             message = "유효하지 않은 회원 로그인 타입입니다."
         )
-        val tokenCredentials = authentication.credentials.toString()
-        val user = userFinder.findByEmailAndLoginType(email, loginType)
+        val tokenCredentials: String = authentication.credentials.toString()
+        val user: User = userFinder.findByEmailAndLoginType(email, loginType)
         when (user.loginType) {
             UserLoginType.GENERAL -> {
                 if (!passwordEncoder.matches(tokenCredentials, user.password)) {
@@ -42,7 +44,7 @@ class CustomAuthenticationManager(
                 }
             }
         }
-        val authorities = AuthorityUtils.createAuthorityList(user.role.name)
+        val authorities: List<GrantedAuthority> = AuthorityUtils.createAuthorityList(user.role.name)
         return UsernamePasswordAuthenticationToken(user.id, user.password, authorities)
     }
 }
