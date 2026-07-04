b55b11da5e23757846666a5a842ab4963f91b66f
chobolevel <rodaka123@naver.com>
2026-07-04 16:44:46 +0900

    refactor: UserQuerydslRepository 불필요한 @sample 주석 제거

--- domain/src/main/kotlin/com/chobolevel/domain/user/repository/UserQuerydslRepository.kt
+++ domain/src/main/kotlin/com/chobolevel/domain/user/repository/UserQuerydslRepository.kt
@@ -11,17 +11,6 @@ import org.springframework.stereotype.Repository
 @Repository
 class UserQuerydslRepository : QuerydslRepositorySupport(User::class.java) {
 
-    /**
-     * Get user entities meet the condition by querydsl
-     * @author chobolevel
-     * @see querydsl
-     * @see User
-     * @param predicates Array&lt;BooleanExpression&gt;
-     * @param pagination Pagination(skip: Long, limit: Long)
-     * @param orderSpecifiers Array&lt;OrderSpecifier&lt;*&gt;&gt;
-     * @return List&lt;User&gt;
-     * @sample UserFinder.search
-     */
     fun searchByPredicates(
         predicates: Array<BooleanExpression>,
         pagination: Pagination,
@@ -35,15 +24,6 @@ class UserQuerydslRepository : QuerydslRepositorySupport(User::class.java) {
             .fetch()
     }
 
-    /**
-     * Get user entities total count meet the condition by querydsl
-     * @author chobolevel
-     * @see querydsl
-     * @see User
-     * @param predicates Array&lt;BooleanExpression&gt;
-     * @return Long
-     * @sample UserFinder.searchCount
-     */
     fun countByPredicates(predicates: Array<BooleanExpression>): Long {
         return from(user)
             .where(*predicates)
