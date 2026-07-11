2e43af48f612057d26474d01255323376282f4d0
chobolevel <rodaka123@naver.com>
2026-07-11 17:09:43 +0900

    fix: query param 단일 enum List 역직렬화 오류 수정

--- api/src/main/kotlin/com/chobolevel/api/common/resolver/QueryObjectArgumentResolver.kt
+++ api/src/main/kotlin/com/chobolevel/api/common/resolver/QueryObjectArgumentResolver.kt
@@ -1,6 +1,7 @@
 package com.chobolevel.api.common.resolver
 
 import com.chobolevel.api.common.annotation.QueryObject
+import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.core.MethodParameter
 import org.springframework.web.bind.support.WebDataBinderFactory
@@ -9,8 +10,13 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
 
 class QueryObjectArgumentResolver(
-    private val objectMapper: ObjectMapper
+    objectMapper: ObjectMapper
 ) : HandlerMethodArgumentResolver {
+
+    // 단일 값 query param도 servlet이 Array<String>으로 전달하므로 항상 toList()로 변환한다.
+    // String? 등 scalar 필드에 단일 원소 리스트가 들어올 때 unwrap하기 위해 전역 ObjectMapper를 복사해 사용한다.
+    private val mapper: ObjectMapper = objectMapper.copy()
+        .enable(DeserializationFeature.UNWRAP_SINGLE_VALUE_ARRAYS)
 
     override fun supportsParameter(parameter: MethodParameter): Boolean =
         parameter.hasParameterAnnotation(QueryObject::class.java)
@@ -23,9 +29,7 @@ class QueryObjectArgumentResolver(
     ): Any? {
         val paramMap: Map<String, Any> = webRequest.parameterMap
             .entries
-            .associate { (key, values) ->
-                key to if (values.size == 1) values[0] else values.toList()
-            }
-        return objectMapper.convertValue(paramMap, parameter.parameterType)
+            .associate { (key, values) -> key to values.toList() }
+        return mapper.convertValue(paramMap, parameter.parameterType)
     }
 }
