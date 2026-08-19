32d78878fa1bd0f4225846cdea78b8c7d206ca53
chobolevel <rodaka123@naver.com>
2026-08-19 18:31:55 +0900

    test: UserControllerTest 미사용 import 제거

--- api/src/test/kotlin/com/chobolevel/api/user/controller/UserControllerTest.kt
+++ api/src/test/kotlin/com/chobolevel/api/user/controller/UserControllerTest.kt
@@ -28,7 +28,6 @@ import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put
