54bd8074ebd4a2d4220876f552c66efa20fffb2c
chobolevel <rodaka123@naver.com>
2026-09-18 18:27:26 +0900

    feat: 팔로우 회원 테이블 마이그레이션 파일 추가

---
+++ domain/src/main/resources/db/migration/V9__add_user_follows_table.sql
@@ -0,0 +1,12 @@
+create table log.user_follows
+(
+  id                 bigint    auto_increment
+      primary key comment '아이디',
+  follower_user_id   bigint    not null comment '팔로워 회원 아이디',
+  following_user_id  bigint    not null comment '팔로잉 회원 아이디',
+  created_at         datetime  not null comment '등록일시',
+  constraint fk_user_follows_follower_user_id foreign key (follower_user_id) references log.users (id) on delete cascade,
+  constraint fk_user_follows_following_user_id foreign key (following_user_id) references log.users (id) on delete cascade,
+  index idx_user_follows_follower_user_id (follower_user_id),
+  index idx_user_follows_following_user_id (following_user_id)
+) comment '회원 팔로우 테이블';
