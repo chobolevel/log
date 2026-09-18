611382fd792ed7e8554b8ea9bf679f795a6a0cb3
chobolevel <rodaka123@naver.com>
2026-09-18 18:27:43 +0900

    feat: 팔로우 회원 엔티티 작성

---
+++ domain/src/main/kotlin/com/chobolevel/domain/user/follow/entity/UserFollow.kt
@@ -0,0 +1,48 @@
+package com.chobolevel.domain.user.follow.entity
+
+import com.chobolevel.domain.user.entity.User
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import java.time.OffsetDateTime
+
+@Entity
+@Table(name = "user_follows")
+class UserFollow private constructor(
+    followingUser: User,
+    followerUser: User
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_user_id", nullable = false, updatable = false)
+    val followingUser: User = followingUser
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_user_id", nullable = false, updatable = false)
+    val followerUser: User = followerUser
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    lateinit var createdAt: OffsetDateTime
+
+    companion object {
+        internal fun create(followingUser: User, followerUser: User): UserFollow {
+            require(followingUser.id == followerUser.id) { "팔로잉, 팔로워 회원이 같습니다." }
+            return UserFollow(
+                followingUser = followingUser,
+                followerUser = followerUser
+            )
+        }
+    }
+}
